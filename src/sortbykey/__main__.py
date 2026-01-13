070cd17b6258ae879230a77571f03221e6ba0c24
Willow Ciesialka <wciesialka@gmail.com>
2026-01-12 21:45:28 -0600

    __main__ now starts work through workmanager module

--- src/sortbykey/__main__.py
+++ src/sortbykey/__main__.py
@@ -1,5 +1,13 @@
+import asyncio
+from sortbykey import cli
+from sortbykey import workmanager
+
 def main():
-    print("Hello, world!")
+    args = cli.parse_args()
+    input_dir = args.input
+    output_dir = args.output
+    num_workers = args.jobs
+    asyncio.run(workmanager.start_work(input_dir, output_dir, num_workers))
 
 if __name__ == "__main__":
     main()
