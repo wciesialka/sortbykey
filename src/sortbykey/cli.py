c1b30b3573d806d271d6009c45f2871e41f4a3c9
Willow Ciesialka <wciesialka@gmail.com>
2026-05-19 18:18:25 -0500

    Updated help message and README

--- src/sortbykey/cli.py
+++ src/sortbykey/cli.py
@@ -38,15 +38,15 @@ def get_argparser() -> argparse.ArgumentParser:
         description="Sort audio files by key."
     )
 
-    parser.add_argument('-i', '--input', required=True, help="Input directory for the unsorted audio files.", type=readable_directory)
-    parser.add_argument("-o", "--output", required=True, help="Output directory for the sorted audio files.", type=writeable_directory)
+    parser.add_argument('-i', '--input', required=True, help="Required. Input directory for the unsorted audio files.", type=readable_directory)
+    parser.add_argument("-o", "--output", required=True, help="Required. Output directory for the sorted audio files.", type=writeable_directory)
 
     num_cores = os.cpu_count() or 1
     num_workers = (num_cores - 1) if num_cores > 1 else 1
 
-    parser.add_argument("-j", "--jobs", type=positive_nonzero_int, default=num_workers, help="Number of concurrent jobs to run for analyzing.")
+    parser.add_argument("-j", "--jobs", type=positive_nonzero_int, default=num_workers, help="Number of concurrent jobs to run for analyzing. Defaults to all cores minus one.")
 
-    parser.add_argument("-c", "--copy", action="store_true", help="Specify this flag to copy files instead of moving them.")
+    parser.add_argument("-c", "--copy", action="store_true", help="Specify this flag to copy files instead of creating links to them.")
 
     return parser
 
